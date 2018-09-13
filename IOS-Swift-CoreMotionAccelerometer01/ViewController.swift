2a5189e32eb4fb57615e54ec75b95e081c4ef0f0
Pooya Hatami <pooya@node-1w7jr9qro06sw04hcipbybhw2.ipv6.telus.net>
2018-09-13 00:26:48 -0700

    done

--- IOS-Swift-CoreMotionAccelerometer01/ViewController.swift
+++ IOS-Swift-CoreMotionAccelerometer01/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var XAccel: UITextField!
+    @IBOutlet weak var yAccel: UITextField!
+    @IBOutlet weak var zAccel: UITextField!
+    
+    var motion = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myAccelerometer()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func myAccelerometer() {
+        print("Start Accelerometer")
+        motion.accelerometerUpdateInterval = 0.5
+        motion.startAccelerometerUpdates(to: OperationQueue.current!) {
+            (data, error) in
+            print(data as Any)
+            if let trueData =  data {
+                
+                self.view.reloadInputViews()
+                let x = trueData.acceleration.x
+                let y = trueData.acceleration.y
+                let z = trueData.acceleration.z
+                self.XAccel!.text = "x: \(Double(x).rounded(toPlaces :3))"
+                self.yAccel!.text = "y: \(Double(y).rounded(toPlaces :3))"
+                self.zAccel!.text = "z: \(Double(z).rounded(toPlaces :3))"
+            }
+        }
+        
+        return
     }
 
 
 }
 
+
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func rounded(toPlaces places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
